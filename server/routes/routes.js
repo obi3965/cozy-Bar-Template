bd6d37fe3969f9671b836ff6b70dba9a3875482b
obaidullah obi <obaidullahahmad3965@gmail.com>
2019-06-05 14:53:35 +0200

    commit

---
+++ server/routes/routes.js
@@ -0,0 +1,40 @@
+
+module.exports = (app) => {
+
+    
+    app.get('/', function(req,res){
+       res.render('home.ejs',{
+          "title":"home"
+       })
+    })
+    
+    app.get('/events', function(req,res, next){
+       res.render('events.ejs',{
+          "title":"events",
+          
+          
+       })
+ 
+       
+    })
+    
+    app.get('/quiz', function(req, res, next){
+       res.render('quiz.ejs',{
+          "title":"quiz"
+       })
+    })
+    
+    
+    app.get('/contact', function(req, res, next){
+       res.render('contact.ejs',{
+          "title":"contact"
+       })
+    })
+ 
+        
+    // app.get('/', (req, res, next) => {
+    //    let now = new Date('2019-01-14 07:00:14');
+    //    console.log(app.locals.dateAndTime.format(now, 'h:mm A | MMMM DD'));
+    // });
+ 
+ };
